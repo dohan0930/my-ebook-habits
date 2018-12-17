c44c1533e9a051cb51c9cc6e2db39ce61f49a1b4
Dongwon Jang <dohan0930@gmail.com>
2018-12-17 23:15:16 +0900

    try to find login field value

--- purchase_datacrawler/datacrawler.py
+++ purchase_datacrawler/datacrawler.py
@@ -2,7 +2,18 @@
 #crawl my purchase data from RIDIBOOKS(https://ridibooks.com/library/)
 
 import requests
-URL = 'https://ridibooks.com/library'
-response = requests.get(URL)
-print(response.status_code)
-print(response.text)
+from bs4 import BeautifulSoup as bs
+
+URL = 'https://ridibooks.com/account/login?return_url=http%3A%2F%2Fridibooks.com%2Flibrary%2F'
+
+LOGIN_INFO = {
+		'user_id' : 'dohan0930',
+		'password' : '######################',
+#		'auto_login' : '1'
+}
+
+with requests.Session() as login_try :
+	login_request = login_try.post(URL, data=LOGIN_INFO, allow_redirects=False)
+	print(login_request.status_code)
+	print(login_request.cookies)
+	print(login_request.text)
