331a65c5f8a10804cde8e0543d517036539f0f72
Dongwon Jang <dohan0930@gmail.com>
2018-12-18 23:45:10 +0900

    How can I login???

--- purchase_datacrawler/datacrawler.py
+++ purchase_datacrawler/datacrawler.py
@@ -2,18 +2,23 @@
 #crawl my purchase data from RIDIBOOKS(https://ridibooks.com/library/)
 
 import requests
-from bs4 import BeautifulSoup as bs
+#from requests.auth import HTTPBasicAuth
 
-URL = 'https://ridibooks.com/account/login?return_url=http%3A%2F%2Fridibooks.com%2Flibrary%2F'
+URL = 'https://ridibooks.com/account/login'
+# print(requests.get(URL, auth=HTTPBasicAuth('dohan0930', '####')))
 
 LOGIN_INFO = {
+		'cmd' : 'login',
+		'return_url' : '/?genre=general',
+		'return_query_string' : '',
 		'user_id' : 'dohan0930',
-		'password' : '######################',
-#		'auto_login' : '1'
+		'password' : '',
+#		'device_id' : '',
+#		'msg' : ''
 }
 
 with requests.Session() as login_try :
-	login_request = login_try.post(URL, data=LOGIN_INFO, allow_redirects=False)
+	login_request = login_try.post(URL, data=LOGIN_INFO)
 	print(login_request.status_code)
 	print(login_request.cookies)
 	print(login_request.text)
